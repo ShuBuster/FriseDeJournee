5d6d9a5e04d42314cf7e13576108c85cabed5b97
pc <pc@VICTOR.VICTOR>
2015-03-17 19:29:55 +0100

    resolution pull

--- gen/atlas/frisedejournee/R.java
+++ gen/atlas/frisedejournee/R.java
@@ -16,20 +16,19 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int blanc=0x7f050001;
-        public static final int blanc_casse=0x7f05000d;
-        public static final int bleu1=0x7f050005;
-        public static final int bleu2=0x7f050006;
-        public static final int fushia=0x7f05000c;
-        public static final int jaune1=0x7f050007;
-        public static final int orange1=0x7f050008;
-        public static final int orange2=0x7f050009;
-        public static final int orange3=0x7f05000a;
-        public static final int rose=0x7f05000b;
+        public static final int blanc_casse=0x7f05000c;
+        public static final int bleu1=0x7f050004;
+        public static final int bleu2=0x7f050005;
+        public static final int fushia=0x7f05000b;
+        public static final int jaune1=0x7f050006;
+        public static final int orange1=0x7f050007;
+        public static final int orange2=0x7f050008;
+        public static final int orange3=0x7f050009;
+        public static final int rose=0x7f05000a;
         public static final int translucide=0x7f050000;
-        public static final int vert1=0x7f050002;
-        public static final int vert2=0x7f050003;
-        public static final int vert3=0x7f050004;
+        public static final int vert1=0x7f050001;
+        public static final int vert2=0x7f050002;
+        public static final int vert3=0x7f050003;
     }
     public static final class drawable {
         public static final int back=0x7f020000;
@@ -71,46 +70,42 @@ public final class R {
         public static final int u=0x7f020024;
     }
     public static final class id {
-        public static final int bouton_aide=0x7f08000f;
-        public static final int bouton_left=0x7f080012;
-        public static final int bouton_manual=0x7f080011;
-        public static final int bouton_menu=0x7f08000e;
-        public static final int bouton_retour=0x7f080001;
-        public static final int bouton_right=0x7f080005;
-        public static final int d=0x7f08001a;
-        public static final int e=0x7f080019;
-        public static final int e2=0x7f08001b;
-        public static final int e3=0x7f080021;
-        public static final int e4=0x7f080022;
-        public static final int f=0x7f080015;
-        public static final int frame=0x7f080004;
-        public static final int frise=0x7f080003;
-        public static final int go=0x7f080013;
-        public static final int heure=0x7f080008;
-        public static final int heure_dizaine=0x7f080009;
-        public static final int heure_unite=0x7f08000a;
-        public static final int i=0x7f080017;
-        public static final int imageTask=0x7f080007;
-        public static final int j=0x7f08001c;
-        public static final int menuDeroulant=0x7f080002;
-        public static final int minute_dizaine=0x7f08000b;
-        public static final int minute_unite=0x7f08000c;
-        public static final int n=0x7f080020;
-        public static final int o=0x7f08001d;
-        public static final int r=0x7f080016;
-        public static final int r2=0x7f08001f;
-        public static final int s=0x7f080018;
-        public static final int scope=0x7f080010;
-        public static final int texte=0x7f08000d;
-        public static final int texte_aide=0x7f080000;
-        public static final int titre=0x7f080014;
-        public static final int titreTask=0x7f080006;
-        public static final int u=0x7f08001e;
+        public static final int bouton_aide=0x7f08000a;
+        public static final int bouton_left=0x7f08000e;
+        public static final int bouton_manual=0x7f08000d;
+        public static final int bouton_menu=0x7f080009;
+        public static final int bouton_retour=0x7f08000b;
+        public static final int bouton_right=0x7f080004;
+        public static final int d=0x7f080016;
+        public static final int e=0x7f080015;
+        public static final int e2=0x7f080017;
+        public static final int e3=0x7f08001d;
+        public static final int e4=0x7f08001e;
+        public static final int f=0x7f080011;
+        public static final int frame=0x7f080003;
+        public static final int frise=0x7f080002;
+        public static final int go=0x7f08000f;
+        public static final int heure=0x7f080007;
+        public static final int i=0x7f080013;
+        public static final int imageTask=0x7f080006;
+        public static final int j=0x7f080018;
+        public static final int menuDeroulant=0x7f080000;
+        public static final int n=0x7f08001c;
+        public static final int o=0x7f080019;
+        public static final int r=0x7f080012;
+        public static final int r2=0x7f08001b;
+        public static final int s=0x7f080014;
+        public static final int scope=0x7f08000c;
+        public static final int texte=0x7f080008;
+        public static final int texte_aide=0x7f080001;
+        public static final int titre=0x7f080010;
+        public static final int titreTask=0x7f080005;
+        public static final int u=0x7f08001a;
     }
     public static final class layout {
-        public static final int activity_aide=0x7f030000;
-        public static final int activity_frise=0x7f030001;
-        public static final int activity_menu=0x7f030002;
+        public static final int activity_frise=0x7f030000;
+        public static final int activity_menu=0x7f030001;
+        public static final int menu_aide=0x7f030002;
     }
     public static final class string {
         public static final int aide=0x7f060003;
